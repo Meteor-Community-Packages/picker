0fcc16e5ed1f1745186c1674de7e523265faaf41
Kelly Copley <copleykj@gmail.com>
2024-09-28 16:22:04 -0400

    updates for Meteor V3

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'communitypackages:picker',
   summary: 'Server Side Router for Meteor',
-  version: '1.2.0',
+  version: '2.0.0-beta.0',
   git: 'https://github.com/Meteor-Community-Packages/picker.git',
   documentation: 'README.md',
   deprecated: true
@@ -12,7 +12,7 @@ Npm.depends({
 });
 
 function configurePackage(api) {
-  api.versionsFrom(['1.9']);
+  api.versionsFrom('METEOR@3.0.1');
   api.use(['webapp', 'ecmascript', 'url'], 'server');
 }
 
@@ -24,6 +24,7 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   configurePackage(api);
   api.use('communitypackages:picker', 'server');
-  api.use(['tinytest', 'fetch', 'random'], 'server');
+  api.use(['meteortesting:browser-tests@1.7.0', 'meteortesting:mocha@3.2.0']);
+  api.use(['fetch', 'random'], 'server');
   api.mainModule('test/instance.js', 'server');
 });
--- test/instance.js
+++ test/instance.js
@@ -1,160 +1,177 @@
-import { Picker } from 'meteor/communitypackages:picker'
-import { fetch } from 'meteor/fetch'
-import { Meteor } from 'meteor/meteor'
-import { Random } from 'meteor/random'
+import { Picker } from 'meteor/communitypackages:picker';
+// TODO replace HTTP with fetch
+import { HTTP } from 'meteor/http';
+import { Random } from 'meteor/random';
+import { Meteor } from 'meteor/meteor';
+import { fetch } from 'meteor/fetch';
+import { check } from 'meteor/check';
+import { should, assert } from 'chai';
 
-function getPath(path) {
+should();
+function getPath (path) {
   return Meteor.absoluteUrl(path);
 }
 
-async function getAsync (url, options) {
-  try {
-    const response = await fetch(url, options)
-    return await response.text()
-  } catch (e) {
-    throw new Meteor.Error(500, e.message)
-  }
-}
 
-const get = Meteor.wrapAsync(getAsync)
 
-Tinytest.add('normal route', function(test) {
-  const id = Random.id();
-  Picker.route(`/${id}`, function(params, req, res) {
-    res.end("done");
+describe('picker - basic routes', function () {
+  it('should handle a simple route', async function () {
+    const id = Random.id();
+    Picker.route(`/${id}`, function (params, req, res) {
+      res.end("done");
+    });
+
+    const res = await fetch(getPath(id));
+    const content = await res.text();
+    content.should.equal('done');
   });
 
-  get(getPath(id), { method: 'GET' }, (error, success) => {
-    test.equal(success, 'done');
+  it('should handle a simple route with params', async function () {
+    const id = Random.id();
+    const path = "/post/:id";
+    Picker.route(path, function (params, req, res) {
+      res.end(params.id);
+    });
+
+    const res = await fetch(getPath(`post/${id}`));
+    const content = await res.text();
+    content.should.equal(id);
   });
 
+  it('should handle a route that filters all but POST requests', async function () {
+    const id = Random.id();
+    const postRoutes = Picker.filter(function (req, res) {
+      return req.method === "POST";
+    });
+
+    postRoutes.route(`/${id}`, function (params, req, res) {
+      res.end("done");
+    });
+
+
+    const res1 = await fetch(getPath(`/${id}`));
+    const content1 = await res1.text();
+    content1.should.not.equal("done");
+
+    const res2 = await fetch(getPath(`/${id}`), { method: "POST"});
+    const content2 = await res2.text();
+    content2.should.equal("done");
+  });
+
+  it('should handle a route with query strings', async function () {
+    const id = Random.id();
+    Picker.route(`/${id}`, function (params, req, res) {
+      res.end("" + params.query.aa);
+    });
+
+    const res = await fetch(getPath(`/${id}?aa=10&bb=10`));
+    const content = await res.text();
+    content.should.equal("10");
+  });
 });
 
-Tinytest.add('with params', function(test) {
-  const id = Random.id();
-  const path = "/post/:id";
-  Picker.route(path, function(params, req, res) {
-    res.end(params.id);
+describe('picker - middleware', function () {
+  it('should handle a basic middleware', async function () {
+    const id = Random.id();
+
+    Picker.middleware(function (req, res, next) {
+      setTimeout(function () {
+        req.middlewarePass = "ok";
+        next();
+      }, 100);
+    });
+
+    Picker.route(`/${id}`, function (params, req, res) {
+      res.end(req.middlewarePass);
+    });
+
+    const res = await fetch(getPath(`/${id}?aa=10`));
+    const content = await res.text();
+    content.should.equal("ok");
   });
 
-  get(getPath(`post/${id}`), { method: 'GET' }, (error, res) => {
-    test.equal(res, id);
+  it('should handle a middleware with params', async function () {
+      const path = `${Random.id()}/coola`;
+
+      const routes = Picker.filter(function (req, res) {
+        const matched = /coola/.test(req.url);
+        return matched;
+      });
+
+      routes.middleware(function (req, res, next) {
+        setTimeout(function () {
+          req.middlewarePass = "ok";
+          next();
+        }, 100);
+      });
+
+      routes.route(`/${path}`, function (params, req, res) {
+        res.end(req.middlewarePass);
+      });
+
+      const res = await fetch(getPath(path));
+      const content = await res.text();
+      content.should.equal("ok");
+  });
+
+  it('should handle middleware with filtered routes', async function () {
+    const path = `${Random.id()}/coola`;
+
+    const routes = Picker.filter(function (req, res) {
+      const matched = /coola/.test(req.url);
+      return matched;
+    });
+
+    routes.middleware(function (req, res, next) {
+      setTimeout(function () {
+        req.middlewarePass = "ok";
+        next();
+      }, 100);
+    });
+
+    routes.route(`/${path}`, function (params, req, res) {
+      res.end(req.middlewarePass);
+    });
+
+    const res = await fetch(getPath(path));
+    const content = await res.text();
+    content.should.equal("ok");
+  });
+
+  it('should handle middleware with several filtered routes', async function () {
+    const path1 = `${Random.id()}/coola`;
+    const path2 = `${Random.id()}/coola`;
+
+    const routes1 = Picker.filter();
+    const routes2 = Picker.filter();
+
+    const increaseResultBy = (i) => (req, res, next) => {
+      setTimeout(function () {
+        req.result = req.result || 0;
+        req.result += i;
+        next();
+      }, 100);
+    };
+
+    routes1.middleware(increaseResultBy(1));
+    routes2.middleware(increaseResultBy(2));
+
+    Picker.middleware(increaseResultBy(10));
+
+    routes1.route(`/${path1}`, function (params, req, res) {
+      res.end(req.result + '');
+    });
+    routes2.route(`/${path2}`, function (params, req, res) {
+      res.end(req.result + '');
+    });
+
+    const res1 = await fetch(getPath(path1));
+    const content1 = await res1.text();
+    content1.should.equal("11");
+
+    const res2 = await fetch(getPath(path2));
+    const content2 = await res2.text();
+    content2.should.equal("12");
   });
 });
 
-Tinytest.add('filter only POST', function(test) {
-  const id = Random.id();
-  const postRoutes = Picker.filter(function(req, res) {
-    return req.method === "POST";
-  });
-
-  postRoutes.route(`/${id}`, function(params, req, res) {
-    res.end("done");
-  });
-
-  get(getPath(`/${id}`), { method: 'GET' }, (error, res) => {
-    test.isFalse(res === "done");
-  });
-
-
-  get(getPath(`/${id}`), { method: 'POST' }, (error, res) => {
-    test.isTrue(res === "done");
-  });
-
-});
-
-Tinytest.add('query strings', function(test) {
-  const id = Random.id();
-  Picker.route(`/${id}`, function(params, req, res) {
-    res.end("" + params.query.aa);
-  });
-
-  get(getPath(`/${id}?aa=10&bb=10`), { method: 'GET' }, (error, res) => {
-    test.equal(res, "10");
-  });
-
-});
-
-Tinytest.add('middlewares', function(test) {
-  const id = Random.id();
-
-  Picker.middleware(function(req, res, next) {
-    setTimeout(function() {
-      req.middlewarePass = "ok";
-      next();
-    }, 100);
-  });
-
-  Picker.route(`/${id}`, function(params, req, res) {
-    res.end(req.middlewarePass);
-  });
-
-  get(getPath(`/${id}?aa=10`), { method: 'GET' }, (error, res) => {
-    test.equal(res, "ok");
-  });
-
-});
-
-Tinytest.add('middlewares - with filtered routes', function(test) {
-  const path = `${Random.id()}/coola`;
-
-  const routes = Picker.filter(function(req, res) {
-    const matched = /coola/.test(req.url);
-    return matched;
-  });
-
-  routes.middleware(function(req, res, next) {
-    setTimeout(function() {
-      req.middlewarePass = "ok";
-      next();
-    }, 100);
-  });
-
-  routes.route(`/${path}`, function(params, req, res) {
-    res.end(req.middlewarePass);
-  });
-
-  get(getPath(path), { method: 'GET' }, (error, res) => {
-    test.equal(res, "ok");
-  });
-
-});
-
-
-Tinytest.add('middlewares - with several filtered routes', function(test) {
-  const path1 = `${Random.id()}/coola`;
-  const path2 = `${Random.id()}/coola`;
-
-  const routes1 = Picker.filter();
-  const routes2 = Picker.filter();
-
-  const increaseResultBy = (i) => (req, res, next) => {
-    setTimeout(function() {
-      req.result = req.result || 0;
-      req.result += i;
-      next();
-    }, 100);
-  };
-
-  routes1.middleware(increaseResultBy(1));
-  routes2.middleware(increaseResultBy(2));
-
-  Picker.middleware(increaseResultBy(10));
-
-  routes1.route(`/${path1}`, function(params, req, res) {
-    res.end(req.result+'');
-  });
-  routes2.route(`/${path2}`, function(params, req, res) {
-    res.end(req.result+'');
-  });
-
-  get(getPath(path1), { method: 'GET' }, (error, res) => {
-    test.equal(res, "11");
-  });
-
-
-  get(getPath(path2), { method: 'GET' }, (error, res) => {
-    test.equal(res, "12");
-  });
-
-});
